c9d74ea2a21e15340de60d34e14a5562ebc168bd
mihail-soltan <mihail.soltan@protonmail.ch>
2023-08-17 21:30:27 +0300

    categories CRUD

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -30,3 +30,24 @@ export const addNewCategory = async (body: CategoryType) => {
     throw new Error(`Failed to add new category: ${err}`);
   }
 };
+
+export const editCategory = async (
+  categoryId: string | undefined,
+  body: CategoryType
+) => {
+  try {
+    const response = await axios.put(`${API_URL}/${categoryId}`, body);
+    return response.data;
+  } catch (err) {
+    throw new Error(`Failed to edit category: ${err}`);
+  }
+};
+
+export const deleteCategory = async (categoryId: string | undefined) => {
+  try {
+    const response = await axios.delete(`${API_URL}/${categoryId}`);
+    return response.data;
+  } catch (err) {
+    throw new Error(`Failed to delete category: ${err}`);
+  }
+};
